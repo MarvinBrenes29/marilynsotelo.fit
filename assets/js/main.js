9066c86813e3e5b94a57bd970df55db94d03efb2
Marvin Brenes <marvin9629@gmail.com>
2026-01-27 16:21:58 -0600

    fix: scroll smooth

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,18 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener("load", () => {
   const hash = window.location.hash;
-  if (!hash) return;
+  if (hash) {
+    setTimeout(() => {
+      const target = document.querySelector(hash);
+      const nav = document.querySelector("nav");
+      
+      if (target) {
+        const offset = nav ? nav.offsetHeight : 0;
+        const y = target.getBoundingClientRect().top + window.scrollY - offset - 20;
 
-  const target = document.querySelector(hash);
-  const nav = document.querySelector("nav");
-
-  if (!target || !nav) return;
-
-  const offset = nav.offsetHeight;
-  const y = target.getBoundingClientRect().top + window.scrollY - offset - 20;
-
-  window.scrollTo({
-    top: y,
-    behavior: "smooth"
-  });
+        window.scrollTo({
+          top: y,
+          behavior: "smooth"
+        });
+      }
+    }, 100); 
+  }
 });
