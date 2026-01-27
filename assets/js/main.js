0d4abe280add8f8a75ab5e31c5544621369f45b8
Marvin Brenes <marvin9629@gmail.com>
2026-01-27 15:52:59 -0600

    feat: scroll smooth

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
   initPricing();
   initProducts();
 });
+
+window.addEventListener("load", () => {
+  const hash = window.location.hash;
+  if (!hash) return;
+
+  const target = document.querySelector(hash);
+  const nav = document.querySelector("nav");
+
+  if (!target || !nav) return;
+
+  const offset = nav.offsetHeight;
+  const y = target.getBoundingClientRect().top + window.scrollY - offset - 20;
+
+  window.scrollTo({
+    top: y,
+    behavior: "smooth"
+  });
+});
